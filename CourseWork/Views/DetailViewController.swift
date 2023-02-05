a834f6c533546d3e9f0fdaf4a91b5d3a94d890c8
Lekhana Sandeep <lekhanasandeep2018@gmail.com>
2023-02-05 16:46:46 +0530

    Detail Page 1st Commit

--- CourseWork/Views/DetailViewController.swift
+++ CourseWork/Views/DetailViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import SnapKit
 
-
 class DetailViewController: UIViewController {
 
     static let identifier = "TweerViewController"
@@ -17,8 +16,8 @@ class DetailViewController: UIViewController {
 
     let poster : UIImageView = {
         let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
 
@@ -27,21 +26,30 @@ class DetailViewController: UIViewController {
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 30, weight: .bold)
         label.textColor = .red
+        label.text = "Food Name"
+        label.numberOfLines = 0
         return label
     }()
     
     private let foodDetail : UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20, weight: .regular)
         label.numberOfLines = 0
         return label
     }()
+    
     
     let sugarLevel : UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 18, weight: .regular)
-        label.textColor = .white
+        label.textColor = .black
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        label.layer.cornerCurve = .circular
+        label.layer.cornerRadius = 30
+        label.layer.masksToBounds = true
         label.numberOfLines = 0
         return label
     }()
@@ -50,7 +58,14 @@ class DetailViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 18, weight: .regular)
-        label.textColor = .white
+        label.textColor = .black
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        label.layer.cornerCurve = .circular
+        label.layer.cornerRadius = 30
+        label.layer.masksToBounds = true
+        label.numberOfLines = 0
+        label.text = "Salt"
         label.numberOfLines = 0
         return label
     }()
@@ -59,8 +74,15 @@ class DetailViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 18, weight: .regular)
-        label.textColor = .white
+        label.textColor = .black
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        label.layer.cornerCurve = .circular
+        label.layer.cornerRadius = 30
+        label.layer.masksToBounds = true
         label.numberOfLines = 0
+        label.numberOfLines = 0
+        
         return label
     }()
     
@@ -68,7 +90,13 @@ class DetailViewController: UIViewController {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = .systemFont(ofSize: 18, weight: .regular)
-        label.textColor = .white
+        label.textColor = .black
+        label.textAlignment = .center
+        label.backgroundColor = .white
+        label.layer.cornerCurve = .circular
+        label.layer.cornerRadius = 30
+        label.layer.masksToBounds = true
+        label.numberOfLines = 0
         label.numberOfLines = 0
         return label
     }()
@@ -77,14 +105,22 @@ class DetailViewController: UIViewController {
         let btn = UIButton()
         btn.translatesAutoresizingMaskIntoConstraints = false
         btn.setTitle("Add to Favorite", for: .normal)
-        btn.backgroundColor = .white
-        btn.setTitleColor(.systemPink, for: .normal)
+        btn.backgroundColor = .systemBlue
+        btn.setTitleColor(.white, for: .normal)
+        btn.layer.cornerRadius = 25
         return btn
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        poster.image = UIImage(named: "pizza")
+        view.addSubview(foodname)
+        view.addSubview(foodDetail)
+        view.addSubview(sugarLevel)
+        view.addSubview(saltLevel)
+        view.addSubview(fatLevel)
+        view.addSubview(caloriesLevel)
+        view.addSubview(poster)
+        view.addSubview(button)
 
         self.view.backgroundColor = .systemBackground
         setupConstraint()
@@ -103,27 +139,96 @@ class DetailViewController: UIViewController {
     }
 
     func setupConstraint(){
-        self.view.addSubview(poster)
-        poster.snp.makeConstraints{ make in
-            make.top.equalToSuperview()
-            make.height.equalTo(400)
-            make.leading.trailing.equalToSuperview()
-        }
-        let holder = UIStackView(arrangedSubviews: [foodname, foodDetail, sugarLevel, saltLevel, fatLevel, caloriesLevel])
-        holder.spacing = 10
-        holder.axis = .vertical
-        self.view.addSubview(holder)
+//        self.view.addSubview(poster)
+//        poster.snp.makeConstraints{ make in
+//            make.top.equalToSuperview()
+//            make.height.equalTo(400)
+//            make.leading.trailing.equalToSuperview()
+//        }
+//        let holder = UIStackView(arrangedSubviews: [foodnameLabel, foodname, foodDetail,  sugarLevel, saltLevel, fatLevel, caloriesLevel])
+//        holder.spacing = 10
+//        holder.axis = .vertical
+//        self.view.addSubview(holder)
+//
+//        holder.snp.makeConstraints{ make in
+//            make.top.equalTo(poster.snp_bottomMargin).offset(40)
+//            make.leading.equalTo(view.snp_leadingMargin).offset(20)
+//            make.trailing.equalTo(view.snp_trailingMargin).offset(-20)
+//        }
+//
+//
+//        self.view.addSubview(button)
+//        button.snp.makeConstraints {make in
+//            make.leading.equalToSuperview().offset(20)
+//            make.bottom.equalToSuperview().offset(-100)
+//            make.trailing.equalToSuperview().offset(-20)
+//        }
 
-        holder.snp.makeConstraints{ make in
-            make.top.equalTo(poster.snp_bottomMargin).offset(40)
-            make.leading.equalTo(view.snp_leadingMargin).offset(20)
-            make.trailing.equalTo(view.snp_trailingMargin).offset(-20)
-        }
-        self.view.addSubview(button)
-        button.snp.makeConstraints {make in
-            make.leading.equalToSuperview().offset(20)
-            make.bottom.equalToSuperview().offset(-100)
-            make.trailing.equalToSuperview().offset(-20)
-        }
+        
+        let avatarImageViewConstraints = [
+            poster.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            poster.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            poster.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            poster.heightAnchor.constraint(equalToConstant: 5)
+        ]
+
+        let foodNameConstraints = [
+            foodname.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            foodname.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            foodname.topAnchor.constraint(equalTo: poster.bottomAnchor, constant: 120),
+            foodname.heightAnchor.constraint(equalToConstant: 30)
+        ]
+
+        let foodDetailConstraints = [
+            foodDetail.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            foodDetail.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            foodDetail.topAnchor.constraint(equalTo: foodname.bottomAnchor, constant: 20),
+            foodDetail.heightAnchor.constraint(equalToConstant: 30)
+        ]
+
+        let sugarLevelConstraints = [
+            sugarLevel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            sugarLevel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            sugarLevel.topAnchor.constraint(equalTo: foodDetail.bottomAnchor, constant: 40),
+            sugarLevel.heightAnchor.constraint(equalToConstant: 40)
+        ]
+
+        let saltLevelConstraints = [
+            saltLevel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            saltLevel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            saltLevel.topAnchor.constraint(equalTo: sugarLevel.bottomAnchor, constant: 20),
+            saltLevel.heightAnchor.constraint(equalToConstant: 40)
+        ]
+
+        let fatLevelConstraints = [
+            fatLevel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            fatLevel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            fatLevel.topAnchor.constraint(equalTo: saltLevel.bottomAnchor, constant: 20),
+            fatLevel.heightAnchor.constraint(equalToConstant: 40)
+        ]
+
+        let caloriesLevelConstraints = [
+            caloriesLevel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            caloriesLevel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            caloriesLevel.topAnchor.constraint(equalTo: fatLevel.bottomAnchor, constant: 20),
+            caloriesLevel.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        let buttonConstraints = [
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            button.topAnchor.constraint(equalTo: caloriesLevel.bottomAnchor, constant: 40),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ]
+
+        NSLayoutConstraint.activate(avatarImageViewConstraints)
+        NSLayoutConstraint.activate(foodNameConstraints)
+        NSLayoutConstraint.activate(foodDetailConstraints)
+        NSLayoutConstraint.activate(sugarLevelConstraints)
+        NSLayoutConstraint.activate(saltLevelConstraints)
+        NSLayoutConstraint.activate(fatLevelConstraints)
+        NSLayoutConstraint.activate(caloriesLevelConstraints)
+        NSLayoutConstraint.activate(buttonConstraints)
+        
     }
 }
